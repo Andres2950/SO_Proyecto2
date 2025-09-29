b7b73769c4cffdb499fde45dc13670a203909ac4
OscarEl+Prron <osobando@estudiantec.cr>
2025-09-28 20:03:51 -0600

    cpp

---
+++ src/algorithms/optimal.h
@@ -0,0 +1,114 @@
+#ifndef OPTIMAL_H
+#define OPTIMAL_H
+
+#include "../data_structures/List.h"
+
+
+int optimal(int k, List<int> &peticiones);
+
+
+#define OPTIMAL_IMPLMENTATION
+#include <stdio.h>
+#include "../data_structures/DLinkedList.h"
+
+/*    peticiones = [3,1,4,8,5,9,3,2,5,4]
+    k = 2
+    cache = peticiones[:k]
+    peticiones = peticiones[k:]
+    fallos = k
+
+    for pe_i, pe in enumerate(peticiones):
+        if pe in cache:
+            print(f"HIT: {pe}")
+            continue
+
+        to_remove_pag = 0
+        to_remove = -1
+        for (pag_i, pag) in enumerate(cache):
+            if (pag not in peticiones[pe_i:]):
+                to_remove_pag = pag_i
+                break
+
+            for j in range(pe_i+1,len(peticiones)):
+                if peticiones[j] == pag:
+                    if to_remove < j:
+                           to_remove = j
+                           to_remove_pag = pag_i
+
+        cache[to_remove_pag] = pe
+        fallos+=1
+        print(f"cache:{cache}") 
+    
+    print(fallos)*
+ * */
+
+int optimal(int k, List<int>& peticiones) {
+    List<int>* cache = new DLinkedList<int>();
+    //Inserta y remueve las cosas hasta que se llene el cache
+    for (int i = 0; i < k; ++i) {
+        peticiones.goToStart();
+        cache->goToStart();
+        cache->append(peticiones.remove());
+    }
+    int fallos = k;
+    printf("Starting cache: ");
+    cache->print();
+    printf("\n");
+
+    // for pe_i, pe in enumerate(peticiones)
+    for (int petition_i = 0; petition_i < peticiones.getSize(); ++petition_i) {
+        peticiones.goToPos(petition_i);
+        // if pe in cache
+        if (cache->contains(peticiones.getElement())) {
+            printf("HIT: %d\n", peticiones.getElement());
+            continue;
+        }
+
+        int to_remove_pag = 0;
+        int to_remove = -1;
+        //for pag_i, pag in enumerate(cache)
+        for (int pag_i = 0; pag_i < cache->getSize(); ++pag_i) {
+            List<int>* tmp = new DLinkedList<int>();
+            // tmp = peticiones[pe_i:]
+            for (int i = petition_i; i < peticiones.getSize(); ++i) {
+                peticiones.goToPos(i);
+                tmp->append(peticiones.getElement()); 
+            }
+            // if pag not int peticiones[pe_i:]
+            cache->goToPos(pag_i);
+            if (!tmp->contains(cache->getElement())){
+                to_remove_pag = pag_i;
+                break;
+            }
+            delete tmp;
+            // for j in range(pe_i+1,len(peticiones)):
+            for (int j = petition_i+1; j < peticiones.getSize(); ++j) {
+                // if peticiones[j] == pag
+                peticiones.goToPos(j);
+                cache->goToPos(pag_i);
+                if (peticiones.getElement() == cache->getElement()) {
+                    if (to_remove < j) {
+                        to_remove = j;
+                        to_remove_pag = pag_i;
+                    }
+                }
+            }
+        }
+        //cache[to_remove_pag] = pe
+        cache->goToPos(to_remove_pag);
+        cache->remove();
+        peticiones.goToPos(petition_i);
+        cache->insert(peticiones.getElement());
+        //fallos+=1
+        ++fallos;
+
+        printf("Cache: ");
+        cache->print();
+        printf("\n");
+    }
+    delete cache;
+    printf("Fallos: %d\n", fallos);
+    return fallos;
+}
+
+#endif // OPTIMAL_H
